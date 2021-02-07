0ff05df7e02823da5ff9fd4b42b8cb949d73b619
Sahil Patel <Patelsaho@gmail.com>
2021-02-07 13:39:31 -0500

    staging server.js, added mongoose connection, and staging to deploy on Heroku

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 // const Place = require("./models/Place");
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/airbub", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false,
-//   useCreateIndex: true,
-// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/airbub", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
 
-// const connection = mongoose.connection;
+const connection = mongoose.connection;
 
-// connection.on("connected", () => {
-//   console.log("Mongoose successfully connected.");
-// });
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected.");
+});
 
-// connection.on("error", (err) => {
-//   console.log("Mongoose connection error: " + err);
-// });
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: " + err);
+});
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // TODO: ABSTRACT THESE PLACES ROUTES OUT INTO A CONTROLLER
 
@@ -45,3 +45,4 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
